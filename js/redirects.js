52a900ec00999c1b92daf1df89c537c28744e039
Christina Saba <155658632+ChristinaNSaba@users.noreply.github.com>
2024-11-11 09:13:28 +0200

    started with the Highlights html and added carousel images.

--- js/redirects.js
+++ js/redirects.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const path = window.location.pathname;
-    const possiblePages = ["index", "bio", "books", "awards", "meetingTheReaders"];
+    const possiblePages = ["index", "bio", "books", "awards", "meetingTheReaders", "highlights"];
     
     possiblePages.forEach(page => {
         if (path === `/${page}`) {

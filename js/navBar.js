c7768ea196c4733e21713c2bb40b79a80dc9d973
Christina Saba <155658632+ChristinaNSaba@users.noreply.github.com>
2024-11-11 09:26:17 +0200

    added js for carousel

--- js/navBar.js
+++ js/navBar.js
@@ -2,7 +2,7 @@
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     const menuIcon = document.querySelector('.menu-icon');
-    
+
     // Toggle the active class for the navigation links and menu icon
     navLinks.classList.toggle('active');
     menuIcon.classList.toggle('active');
@@ -14,7 +14,7 @@ const navLinks = document.querySelectorAll('.nav-links li a');
 
 // Add a click event listener to each link to highlight the active one
 navLinks.forEach(link => {
-    link.addEventListener('click', function() {
+    link.addEventListener('click', function () {
         // Remove 'active' class from all links
         navLinks.forEach(nav => nav.classList.remove('active'));
 
@@ -36,6 +36,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll(".carousel-item");
+    const totalSlides = slides.length;
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector(".carousel-images").style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
 
 
 
